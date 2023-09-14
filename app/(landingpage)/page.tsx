5ea2eae2050917288059491b24c02fb8efe3a922
Junaid Tariq <muhammadjunaidtariq004@gmail.com>
2023-09-14 13:28:09 +0500

    Not a fully functional but pages and suchhave ben developed

---
+++ app/(landingpage)/page.tsx
@@ -0,0 +1,22 @@
+const LandingPage = () => {
+    return (
+        <div>
+            Landing Page (Unprotected)
+            <div>
+            <Link href="/sign-in">
+            <Button>
+            Login
+            </Button>
+            </Link>
+            
+            <Link href="/sign-up">
+            <Button>
+            Register
+            </Button>
+            </Link>
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
